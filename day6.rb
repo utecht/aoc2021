4987a914bb433c635cafc6f549c7136e0fc60311
Joseph Utecht <thenmal@gmail.com>
2021-12-07 18:30:34 -0600

    rubified

--- day6.rb
+++ day6.rb
@@ -8,19 +8,10 @@ File.read('inputs/6').split(',') do | fish |
 	all_fish[fish.to_i] += 1
 end
 
-while day_count > 0
-	new_fish = Array.new(9, 0)
-	all_fish.each_index do | i |
-		new_target = i - 1
-		if new_target < 0
-			new_target = 6
-			new_fish[8] += all_fish[i]
-		end
-		new_fish[new_target] += all_fish[i]
-	end
-	all_fish = new_fish
-	# puts all_fish.join(',')
-	day_count -= 1
+day_count.times do
+	births = all_fish.shift
+	all_fish[6] += births
+	all_fish.push births
 end
 
 puts all_fish.sum
